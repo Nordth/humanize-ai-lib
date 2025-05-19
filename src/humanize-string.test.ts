71091991f0209e8b0c3aba526223a8cccbd8808c
Nordth <emo-name@hotmail.com>
2025-05-19 23:04:01 +0400

    Update package and format

--- src/humanize-string.test.ts
+++ src/humanize-string.test.ts
@@ -1,42 +1,49 @@
+import { test, expect } from 'vitest';
+import { humanizeString } from './humanize-string.js';
 
-import { test, expect } from "vitest";
-import { humanizeString } from "./humanize-string.js";
-
-test("Whitespaces", () => {
+test('Whitespaces', () => {
   expect(humanizeString('Hello\u200b\xa0World!  ')).toStrictEqual({
     text: 'Hello World!',
-    count: 4
+    count: 4,
   });
-  expect(humanizeString('Hello\u200b\xa0World!  ', {
-    transformTrailingWhitespace: false
-  })).toStrictEqual({
+  expect(
+    humanizeString('Hello\u200b\xa0World!  ', {
+      transformTrailingWhitespace: false,
+    }),
+  ).toStrictEqual({
     text: 'Hello World!  ',
-    count: 2
+    count: 2,
   });
-  expect(humanizeString('Hello\u200b\xa0World!  ', {
-    transformHidden: false
-  })).toStrictEqual({
+  expect(
+    humanizeString('Hello\u200b\xa0World!  ', {
+      transformHidden: false,
+    }),
+  ).toStrictEqual({
     text: 'Hello\u200b World!',
-    count: 3
+    count: 3,
   });
-  expect(humanizeString('Hello\u200b\xa0World!  ', {
-    transformNbs: false
-  })).toStrictEqual({
+  expect(
+    humanizeString('Hello\u200b\xa0World!  ', {
+      transformNbs: false,
+    }),
+  ).toStrictEqual({
     text: 'Hello\xa0World!',
-    count: 3
+    count: 3,
   });
 });
 
-test("Dashes", () => {
+test('Dashes', () => {
   expect(humanizeString('I — super — man – 💪')).toStrictEqual({
     text: 'I - super - man - 💪',
-    count: 3
+    count: 3,
   });
-})
+});
 
-test("Quotes", () => {
-  expect(humanizeString('Angular “quote” «marks» looks„ like Christmas «« tree')).toStrictEqual({
+test('Quotes', () => {
+  expect(
+    humanizeString('Angular “quote” «marks» looks„ like Christmas «« tree'),
+  ).toStrictEqual({
     text: 'Angular "quote" "marks" looks" like Christmas "" tree',
-    count: 7
+    count: 7,
   });
-})
+});
